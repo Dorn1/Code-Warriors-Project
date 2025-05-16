e4cd1a20fcd928fb802fc34e4046f97958460384
Kuba <janochajakub02@gmail.com>
2025-05-16 17:42:20 +0200

    update

---
+++ src/main/java/pl/summarizer/codewarriorsproject/Exception/TimeCollisionException.java
@@ -0,0 +1,7 @@
+package pl.summarizer.codewarriorsproject.Exception;
+
+public class TimeCollisionException extends RuntimeException {
+    public TimeCollisionException(String message) {
+        super(message);
+    }
+}
--- src/main/java/pl/summarizer/codewarriorsproject/User/UserService.java
+++ src/main/java/pl/summarizer/codewarriorsproject/User/UserService.java
@@ -1,7 +1,28 @@
 package pl.summarizer.codewarriorsproject.User;
 
 import org.springframework.stereotype.Service;
+import pl.summarizer.codewarriorsproject.Exception.AlreadyExistsException;
+import pl.summarizer.codewarriorsproject.Exception.DoesntExistException;
+
+import java.util.List;
 
 @Service
 public class UserService {
+    UserRepository userRepository;
+    public void addUser(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        List<User> users = userRepository.findByUsername(username);
+        if(!userRepository.findByUsername(username).isEmpty()){
+            throw new AlreadyExistsException("Username " + username + " already exists");
+        }
+        userRepository.save(user);
+    }
+    public boolean login(String username, String password) {
+        if(userRepository.findByUsername(username).isEmpty()){
+            throw new DoesntExistException("Username " + username + " does not exist");
+        }
+        return userRepository.findByUsername(username).getFirst().getPassword().equals(password);
+    }
 }
