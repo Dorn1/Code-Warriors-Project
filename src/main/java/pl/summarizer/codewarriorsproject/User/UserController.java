35b7d1bc491a30bbd0d2d058575af1ef4a9af962
Kuba <janochajakub02@gmail.com>
2025-05-16 18:40:19 +0200

    User services

--- src/main/java/pl/summarizer/codewarriorsproject/User/UserController.java
+++ src/main/java/pl/summarizer/codewarriorsproject/User/UserController.java
@@ -1,7 +1,28 @@
 package pl.summarizer.codewarriorsproject.User;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.summarizer.codewarriorsproject.Exception.AlreadyExistsException;
 
 @RestController
 public class UserController {
+    UserService userService;
+
+    @GetMapping(value = "/login")
+    public ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
+        if(userService.login(username, password)) {
+            return ResponseEntity.ok(userService.getUserId(username).toString());
+        }
+         return ResponseEntity.status(401).body("Bad Credentials");
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<String> register(@RequestParam String user, @RequestParam String password) {
+        try{
+            userService.addUser(user, password);
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(409).body(e.getMessage());
+        }
+        return ResponseEntity.ok("User" + user + "registered successfully");
+    }
 }
--- src/main/java/pl/summarizer/codewarriorsproject/User/UserService.java
+++ src/main/java/pl/summarizer/codewarriorsproject/User/UserService.java
@@ -4,25 +4,26 @@ import org.springframework.stereotype.Service;
 import pl.summarizer.codewarriorsproject.Exception.AlreadyExistsException;
 import pl.summarizer.codewarriorsproject.Exception.DoesntExistException;
 
-import java.util.List;
 
 @Service
 public class UserService {
     UserRepository userRepository;
-    public void addUser(String username, String password) {
+    public void addUser(String username, String password) throws AlreadyExistsException {
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
-        List<User> users = userRepository.findByUsername(username);
         if(!userRepository.findByUsername(username).isEmpty()){
             throw new AlreadyExistsException("Username " + username + " already exists");
         }
         userRepository.save(user);
     }
-    public boolean login(String username, String password) {
+    public boolean login(String username, String password) throws DoesntExistException {
         if(userRepository.findByUsername(username).isEmpty()){
             throw new DoesntExistException("Username " + username + " does not exist");
         }
         return userRepository.findByUsername(username).getFirst().getPassword().equals(password);
+    }
+    public Long getUserId(String username) {
+        return userRepository.findByUsername(username).getFirst().getId();
     }
 }
