08f5fb2eda821986d958b4cb8b7e9726f3b640ee
Kuba <janochajakub02@gmail.com>
2025-05-16 19:27:47 +0200

    more CalendarService

--- src/main/java/pl/summarizer/codewarriorsproject/Calendar/UserCalendar/UserCalendar.java
+++ src/main/java/pl/summarizer/codewarriorsproject/Calendar/UserCalendar/UserCalendar.java
@@ -1,11 +1,13 @@
 package pl.summarizer.codewarriorsproject.Calendar.UserCalendar;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import pl.summarizer.codewarriorsproject.Calendar.Week.Week;
+import pl.summarizer.codewarriorsproject.User.User;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -14,4 +16,8 @@ public class UserCalendar {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    private User user;
+    @OneToMany
+    private Set<Week> weekSet = new HashSet<>();
 }
--- src/main/java/pl/summarizer/codewarriorsproject/Calendar/UserCalendar/UserCalendarRepository.java
+++ src/main/java/pl/summarizer/codewarriorsproject/Calendar/UserCalendar/UserCalendarRepository.java
@@ -2,8 +2,12 @@ package pl.summarizer.codewarriorsproject.Calendar.UserCalendar;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.summarizer.codewarriorsproject.User.User;
+
+import java.util.Set;
 
 @Repository
 public interface UserCalendarRepository extends JpaRepository<UserCalendar, Long> {
 
+    Set<UserCalendar> getUserCalendarByUser(User user);
 }
