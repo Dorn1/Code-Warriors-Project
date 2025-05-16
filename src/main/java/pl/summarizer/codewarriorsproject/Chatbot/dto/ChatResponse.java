5ddb5ebe8e9d3946da8ccca8b3526118820cf057
Mykhailo Derkach <my.derkach@gmail.com>
2025-05-16 18:30:24 +0200

    Implemented chat response class

--- src/main/java/pl/summarizer/codewarriorsproject/Chatbot/dto/ChatResponse.java
+++ src/main/java/pl/summarizer/codewarriorsproject/Chatbot/dto/ChatResponse.java
@@ -1,4 +1,19 @@
 package pl.summarizer.codewarriorsproject.Chatbot.dto;
 
 public class ChatResponse {
+    private String reply;
+
+    public ChatResponse() {}
+
+    public ChatResponse(String reply) {
+        this.reply = reply;
+    }
+
+    public String getReply() {
+        return reply;
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
 }
