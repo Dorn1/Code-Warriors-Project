90323be2322b7eff1670e4dda069f2a6269957c6
Kuba <janochajakub02@gmail.com>
2025-05-16 13:15:38 +0200

    Initial commit

---
+++ src/main/java/pl/summarizer/codewarriorsproject/CodeWarriorsProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.summarizer.codewarriorsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWarriorsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeWarriorsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/summarizer/codewarriorsproject/CodeWarriorsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.summarizer.codewarriorsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWarriorsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
